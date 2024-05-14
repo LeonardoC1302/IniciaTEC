bd73048e3cc207e9165c52b06f0485813d3ab808
Leonardo Céspedes <101959981+LeonardoC1302@users.noreply.github.com>
2024-05-13 23:06:02 -0600

    Router

--- Router.php
+++ Router.php
@@ -19,7 +19,7 @@ class Router
 
     public function checkRoutes()
     {
-        $current_url = $_SERVER['PATH_INFO'] ?? '/';
+        $current_url = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
